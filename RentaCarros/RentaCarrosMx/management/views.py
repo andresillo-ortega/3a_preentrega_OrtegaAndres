4f4d34e53a703bdfcc99823d72ba3207a905a031
Andrés Ortega <andres.ortega@arpon.com>
2024-08-21 22:57:49 -0600

    Correccion de base de datos

--- RentaCarros/RentaCarrosMx/management/views.py
+++ RentaCarros/RentaCarrosMx/management/views.py
@@ -6,7 +6,7 @@ from .models import Cliente, Carro, Renta
 from .forms import ClienteForm, CarroForm, RentaForm, BuscarForm
 
 def inicio(request):
-    return render(request, 'gestion/inicio.html')
+    return render(request, 'management/inicio.html')
 
 def agregar_cliente(request):
     if request.method == 'POST':
@@ -16,7 +16,7 @@ def agregar_cliente(request):
             return redirect('inicio')
     else:
         form = ClienteForm()
-    return render(request, 'gestion/agregar_cliente.html', {'form': form})
+    return render(request, 'management/agregar_cliente.html', {'form': form})
 
 def agregar_carro(request):
     if request.method == 'POST':
@@ -26,7 +26,7 @@ def agregar_carro(request):
             return redirect('inicio')
     else:
         form = CarroForm()
-    return render(request, 'gestion/agregar_carro.html', {'form': form})
+    return render(request, 'management/agregar_carro.html', {'form': form})
 
 def agregar_renta(request):
     if request.method == 'POST':
@@ -36,7 +36,7 @@ def agregar_renta(request):
             return redirect('inicio')
     else:
         form = RentaForm()
-    return render(request, 'gestion/agregar_renta.html', {'form': form})
+    return render(request, 'management/agregar_renta.html', {'form': form})
 
 def buscar(request):
     if request.method == 'POST':
@@ -46,11 +46,11 @@ def buscar(request):
             clientes = Cliente.objects.filter(nombre__icontains=query)
             carros = Carro.objects.filter(marca__icontains=query)
             rentas = Renta.objects.filter(cliente__nombre__icontains=query)
-            return render(request, 'gestion/resultados_busqueda.html', {
+            return render(request, 'management/resultados_busqueda.html', {
                 'clientes': clientes,
                 'carros': carros,
                 'rentas': rentas
             })
     else:
         form = BuscarForm()
-    return render(request, 'gestion/buscar.html', {'form': form})
+    return render(request, 'management/buscar.html', {'form': form})
